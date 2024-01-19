c422a7b7a77624ed5d1d1818405e7906c9d81f2a
Sizov <ser-reb@yandex.ru>
2024-01-19 12:29:33 +0400

    создание класса словарь диапазонов

--- src/application/utils/data.py
+++ src/application/utils/data.py
@@ -10,3 +10,33 @@ from sys import path
 
 ROOT_DIR = Path(path[0]).parent.parent
 DATA_DIR = ROOT_DIR / 'data'
+
+
+class DictOfRange(dict):
+    """Описывает словарь диапазонов."""
+    def __init__(self, mappable: dict):
+        for key in mappable:
+            if (
+                    not isinstance(key, tuple)
+                or len(key) != 2
+                or not isinstance(key[0], int)
+                or not isinstance(key[1], int)                    
+            ):
+                raise ValueError('...')
+        super().__init__(mappable)
+
+    def __getitem__(self, key: int):
+        if isinstance(key, int):
+            for left, right in self:
+                if left <= key <= right:
+                    return super().__getitem__((left, right))
+        else:
+            return super().__getitem__(key)
+
+    def get_range(self, key: int) -> tuple[int, int]:
+        if isinstance(key, int):
+            for left, right in self:
+                if left <= key <= right:
+                    return left, right
+        else:
+            raise TypeError
